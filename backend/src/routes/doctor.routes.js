f5a0cf8c866290d85c9b5bca49617effff90371c
Umang Khemka <umangkhemka2005@gmail.com>
2026-01-08 00:22:25 +0530

    removed the history route

--- backend/src/routes/doctor.routes.js
+++ backend/src/routes/doctor.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import {getDocProfile, updateDocProfile} from "../controllers/doctor.controller.js";
 import { authMiddleware,checkRole } from "../middlewares/auth.middleware.js";
-import {createPrescription,updatePrescription,deletePrescription} from "../controllers/prescription.controller.js";
+import {createPrescription,updatePrescription,deletePrescription, getPrescriptions} from "../controllers/prescription.controller.js";
 import {allAppointments, changeStatus, rescheduleAppointment} from "../controllers/appointment.controller.js";
-import { getDoctorHistory } from "../controllers/history.controller.js";
 
 const router = express.Router();
 
+router.get("/all-prescriptions",authMiddleware,checkRole(['doctor']),getPrescriptions);
 router.get("/all-appointments",authMiddleware,checkRole(['doctor']),allAppointments);
 router.put("/doc-profile",authMiddleware,checkRole(['doctor']),updateDocProfile);
 router.get("/my-profile",authMiddleware,checkRole(['doctor']),getDocProfile);
@@ -15,6 +15,5 @@ router.put("/prescription/:prescriptionId",authMiddleware,checkRole(['doctor']),
 router.delete("/prescription/:prescriptionId",authMiddleware,checkRole(['doctor']),deletePrescription);
 router.put("/change-status/:appointmentId",authMiddleware,checkRole(['doctor']),changeStatus);
 router.put("/reschedule/:appointmentId",authMiddleware,checkRole(['doctor']),rescheduleAppointment);
-router.get("/history",authMiddleware,checkRole(['doctor']),getDoctorHistory);
 
 export default router;
