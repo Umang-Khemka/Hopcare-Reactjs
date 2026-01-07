8ae749eaac594ca4f84027899d252785e42bd3a5
Umang Khemka <umangkhemka2005@gmail.com>
2026-01-08 00:23:02 +0530

    added the get route controller for prescription

--- backend/src/controllers/prescription.controller.js
+++ backend/src/controllers/prescription.controller.js
@@ -2,6 +2,98 @@ import Prescription from "../models/prescription.model.js";
 import Appointment from "../models/appointment.model.js";
 import Doctor from "../models/doctor.model.js";
 
+export const getPrescriptions = async (req, res) => {
+  try {
+    const doctorUserId = req.user._id;
+
+    const doctor = await Doctor.findOne({ userId: doctorUserId });
+    if (!doctor) {
+      return res.status(400).json({ message: "Doctor profile not found" });
+    }
+
+    const prescriptions = await Prescription.aggregate([
+      {
+        $match: {
+          doctorId: doctor._id,
+        },
+      },
+
+      {
+        $lookup: {
+          from: "appointments",
+          localField: "appointmentId",
+          foreignField: "_id",
+          as: "appointment",
+        },
+      },
+      { $unwind: "$appointment" },
+
+      {
+        $match: {
+          "appointment.status": "completed",
+        },
+      },
+
+      {
+        $lookup: {
+          from: "patients",
+          localField: "patientId",
+          foreignField: "_id",
+          as: "patient",
+        },
+      },
+      {
+        $unwind: {
+          path: "$patient",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+
+      {
+        $lookup: {
+          from: "users",
+          localField: "patient.userId",
+          foreignField: "_id",
+          as: "patientUser",
+        },
+      },
+      {
+        $unwind: {
+          path: "$patientUser",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+
+      {
+        $addFields: {
+          "patient.name": "$patientUser.name",
+          "patient.email": "$patientUser.email",
+        },
+      },
+
+      {
+        $project: {
+          patientUser: 0,
+          "patient.password": 0,
+        },
+      },
+    ]);
+
+    return res.status(200).json({
+      success: true,
+      count: prescriptions.length,
+      data: prescriptions,
+    });
+  } catch (error) {
+    console.error("Error fetching prescriptions:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch prescriptions",
+    });
+  }
+};
+
+
 export const createPrescription = async (req, res) => {
   try {
     const { appointmentId, medicines, diagnosis, notes } = req.body;
@@ -19,7 +111,8 @@ export const createPrescription = async (req, res) => {
 
     if (appointment.doctorId.toString() !== doctor._id.toString()) {
       return res.status(403).json({
-        message: "You are not allowed to create prescription for this appointment",
+        message:
+          "You are not allowed to create prescription for this appointment",
       });
     }
 
@@ -43,7 +136,6 @@ export const createPrescription = async (req, res) => {
       message: "Prescription created successfully",
       prescription,
     });
-
   } catch (error) {
     console.error("Error creating prescription:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -72,6 +164,18 @@ export const updatePrescription = async (req, res) => {
       });
     }
 
+    const appointment = await Appointment.findById(prescription.appointmentId);
+
+    if(!appointment){
+      return res.status(404).json({message: "Appointmetn not found"});
+    }
+
+    if(appointment.status !== "completed"){
+      return res.status(400).json({
+        message: "Prescription cannot be edited as it is not completed by the patient"
+      });
+    }
+
     if (medicines) prescription.medicines = medicines;
     if (diagnosis) prescription.diagnosis = diagnosis;
     if (notes) prescription.notes = notes;
@@ -82,7 +186,6 @@ export const updatePrescription = async (req, res) => {
       message: "Prescription updated successfully",
       prescription,
     });
-
   } catch (error) {
     console.error("Error updating prescription:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -115,12 +218,8 @@ export const deletePrescription = async (req, res) => {
     return res.status(200).json({
       message: "Prescription deleted successfully",
     });
-
   } catch (error) {
     console.error("Error deleting prescription:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-
-
