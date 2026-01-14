82624f8c05b7a2720e0af3178a83310daf14bde4
Umang Khemka <umangkhemka2005@gmail.com>
2026-01-15 01:26:57 +0530

    all reviews route added

--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createReview, deleteReview, updateReview } from "../controllers/review.controller.js";
+import { createReview, deleteReview, getReviewById, updateReview } from "../controllers/review.controller.js";
 import { authMiddleware,checkRole } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.post("/new-review",authMiddleware,checkRole(['patient']),createReview);
 router.put("/update-review/:reviewId",authMiddleware,checkRole(['patient']),updateReview);
 router.delete("/delete-review/:reviewId",authMiddleware,checkRole(['patient']),deleteReview);
-
+router.get("/all-reviews",authMiddleware,checkRole(['doctor']),getReviewById);
 export default router;
