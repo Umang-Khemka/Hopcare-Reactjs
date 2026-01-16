80b6604a1bfcb5e8722f06ec8bafb345d5e5956b
Umang Khemka <umangkhemka2005@gmail.com>
2026-01-16 17:40:21 +0530

    new route of patient prescription hsitory

--- backend/src/routes/doctor.routes.js
+++ backend/src/routes/doctor.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {getDocProfile, updateDocProfile} from "../controllers/doctor.controller.js";
 import { authMiddleware,checkRole } from "../middlewares/auth.middleware.js";
-import {createPrescription,updatePrescription,deletePrescription, getPrescriptions} from "../controllers/prescription.controller.js";
+import {createPrescription,updatePrescription,deletePrescription, getPrescriptions, getPatientPrescriptionHistory} from "../controllers/prescription.controller.js";
 import {allAppointments, changeStatus, rescheduleAppointment} from "../controllers/appointment.controller.js";
 
 const router = express.Router();
 
+router.get("/prescription/history/:patientId",authMiddleware,checkRole(['doctor']),getPatientPrescriptionHistory);
 router.get("/all-prescriptions",authMiddleware,checkRole(['doctor']),getPrescriptions);
 router.get("/all-appointments",authMiddleware,checkRole(['doctor']),allAppointments);
 router.put("/doc-profile",authMiddleware,checkRole(['doctor']),updateDocProfile);
